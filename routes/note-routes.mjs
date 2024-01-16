648466f3d1e5a958120d91f42b3fa32b94734071
johnabednego <jilimajohnabednego@gmail.com>
2024-01-16 10:53:11 +0000

    routes

---
+++ routes/note-routes.mjs
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import * as noteControllers from '../controllers/note-controllers.mjs';
+
+const router = Router();
+
+// Note routes
+router
+  .get('/',  noteControllers.getNotes)
+  .get('/:noteId',  noteControllers.getNote)
+  .post('/', noteControllers.createNote)
+  .patch('/:noteId',  noteControllers.updateNote)
+  .delete('/:noteId',  noteControllers.deleteNote)
+  .patch('/:noteId/restore', noteControllers.restoreNote)
+  .patch('/:noteId/trash',  noteControllers.trashNote)
+
+export default router;
